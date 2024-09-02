8e5162a72e8c5757634b1e01e1b65b1cecc6bfd1
unknown <refuerzomaj@gmail.com>
2024-09-02 15:15:19 +0800

    First deploy

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.tsx
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+    <header>
+      <h1>JG</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Skills</li>
+          <li>Projects</li>
+          <li>Contact Us</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
